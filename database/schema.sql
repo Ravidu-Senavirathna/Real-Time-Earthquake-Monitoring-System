a5bca891dec9f55a7f7eeae91b7054e2cf96492a
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-03-27 10:08:09 +0530

    updated the schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,22 @@
-CREATE TABLE IF NOT EXISTS earthquakes (
+CREATE TABLE earthquakes (
     id VARCHAR(50) PRIMARY KEY,
+
     magnitude FLOAT,
+    magnitude_type VARCHAR(10),
+
     place TEXT,
+
     event_time TIMESTAMP,
+    updated_time TIMESTAMP,
+
     latitude FLOAT,
     longitude FLOAT,
-    depth FLOAT
+
+    depth FLOAT,
+
+    tsunami INTEGER,
+    significance INTEGER,
+
+    status VARCHAR(20),
+    event_type VARCHAR(20)
 );
