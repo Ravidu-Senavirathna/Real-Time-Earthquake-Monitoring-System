d3d92d4f5e75a8fe174885d547370f420713ba86
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-03-27 09:08:09 +0530

    created a getdata.py to get live data from url and return to df

---
+++ ingestion/get_data.py
@@ -0,0 +1,51 @@
+import requests
+import pandas as pd
+
+# USGS Earthquake API endpoint for all earthquakes in the past hour
+URL = (
+    "https://earthquake.usgs.gov/"
+    "earthquakes/feed/v1.0/summary/all_day.geojson"
+)
+
+
+def get_live_earthquakes():
+
+    # Fetch earthquake data from the USGS API
+    response = requests.get(URL, timeout=10)
+
+    # Handle HTTP errors
+    response.raise_for_status()
+
+    # Parse the JSON response
+    data = response.json()
+
+    earthquakes = []
+
+    for event in data["features"]:
+
+
+        props = event["properties"]
+        geo = event["geometry"]["coordinates"]
+
+        print(event)
+        print("")
+
+        # Append earthquake data to the list
+        earthquakes.append({
+            "id": event["id"],
+            "magnitude": props["mag"],
+            "place": props["place"],
+            "event_time": pd.to_datetime(
+                props["time"],
+                unit="ms"
+            ),
+            "longitude": geo[0],
+            "latitude": geo[1],
+            "depth": geo[2]
+        })
+
+    return pd.DataFrame(earthquakes)
+
+print("Fetching earthquake data...")
+df = get_live_earthquakes()
+print(df.head())
