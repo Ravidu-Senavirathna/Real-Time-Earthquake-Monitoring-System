3d25fca66e3ae440b7760bac5c7d9c5b5c56cfad
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-04-05 13:18:49 +0530

    updated check sanity

--- ingestion/check_data.py
+++ ingestion/check_data.py
@@ -5,7 +5,9 @@ with engine.connect() as conn:
     result = conn.execute(text("""
         SELECT
             COUNT(*) as total,
-            MAX(magnitude) as max_mag
+            MAX(magnitude) as max_mag,
+            MIN(event_time) as earliest,
+            MAX(event_time) as latest
         FROM earthquakes;
     """))
 
