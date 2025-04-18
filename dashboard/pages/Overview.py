e413b5a2e19288faa55253dfd2d165461b66881e
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-04-18 08:51:27 +0530

    updated overviwe code from app.py

--- dashboard/pages/Overview.py
+++ dashboard/pages/Overview.py
@@ -11,3 +11,19 @@ Strongest Earthquake Location
 Tsunami Events
 '''
 
+import streamlit as st
+from dashboard.utils import load_data
+
+st.subheader("🌍 Overview")
+
+df = load_data()
+
+# KPIs
+col1, col2, col3 = st.columns(3)
+
+col1.metric("Total Earthquakes", len(df))
+col2.metric("Max Magnitude", round(df["magnitude"].max(), 2))
+col3.metric("Avg Magnitude", round(df["magnitude"].mean(), 2))
+
+st.subheader("Latest Earthquakes")
+st.dataframe(df.sort_values("event_time", ascending=False).head(10))
