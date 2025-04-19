3f9fe4b56556489b5df40530378bbc877a94706c
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-04-19 09:29:18 +0530

    updated app.py added new created pages

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,69 +1,14 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-from database.db import engine
+
+st.title("Earthquake Monitoring System")
 
 
+# Define your page objects
+page_1 = st.Page("pages/Overview.py", title="Overview")
+page_2 = st.Page("pages/Global_Map.py", title="Global Map")
+page_3 = st.Page("pages/Trends.py", title="Trends")
+page_4 = st.Page("pages/Top_Earthquakes.py", title="Analysis")
 
-st.set_page_config(page_title="Earthquake Monitor", layout="wide")
-
-
-# Title
-st.title("Real-Time Earthquake Monitoring Dashboard")
-@st.cache_data
-def load_data():
-    return pd.read_sql("SELECT * FROM earthquakes", engine)
-
-df = load_data()
-
-
-
-# Sidebar filters
-st.sidebar.header("Filters")
-
-min_mag = st.sidebar.slider("Minimum Magnitude", 0.0, 10.0, 5.0)
-filtered = df[df["magnitude"] >= min_mag]
-
-
-
-# Display key metrics
-
-col1, col2, col3 = st.columns(3)
-
-col1.metric("Total Earthquakes", len(filtered))
-col2.metric("Max Magnitude", filtered["magnitude"].max())
-col3.metric("Avg Magnitude", round(filtered["magnitude"].mean(), 2))
-
-# Magnitude distribution
-st.subheader("Magnitude Distribution")
-
-fig = px.histogram(filtered, x="magnitude", nbins=40)
-st.plotly_chart(fig, width='stretch')
-
-
-# World map of earthquakes
-st.subheader("Global Earthquake Map")
-
-fig_map = px.scatter_geo(
-    filtered,
-    lat="latitude",
-    lon="longitude",
-    color="magnitude",
-    size="magnitude",
-    hover_name="place",
-    projection="natural earth"
-)
-
-st.plotly_chart(fig_map, width='stretch')
-
-
-# Time series of earthquakes
-filtered["event_time"] = pd.to_datetime(filtered["event_time"])
-
-time_df = filtered.groupby(filtered["event_time"].dt.date).size().reset_index()
-time_df.columns = ["date", "count"]
-
-st.subheader("Earthquakes Over Time")
-
-fig2 = px.line(time_df, x="date", y="count")
-st.plotly_chart(fig2, width='stretch')
+# Pass pages into st.navigation
+nav = st.navigation([page_1, page_2, page_3, page_4])
+nav.run()
