bd880d1081da1ae9629aa866c017bf758ffa8205
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-04-21 08:27:20 +0530

    added 3 more kpi for overviwe

--- dashboard/pages/Overview.py
+++ dashboard/pages/Overview.py
@@ -13,10 +13,15 @@ Tsunami Events
 
 import streamlit as st
 from dashboard.utils import load_data
+import pandas as pd
 
 st.subheader("🌍 Overview")
 
 df = load_data()
+
+strongest = df.loc[df["magnitude"].idxmax()]
+tsunami_count = df[df["tsunami"] == 1].shape[0]
+reviewed = df[df["status"] == "reviewed"].shape[0]
 
 # KPIs
 col1, col2, col3 = st.columns(3)
@@ -24,6 +29,12 @@ col1, col2, col3 = st.columns(3)
 col1.metric("Total Earthquakes", len(df))
 col2.metric("Max Magnitude", round(df["magnitude"].max(), 2))
 col3.metric("Avg Magnitude", round(df["magnitude"].mean(), 2))
+
+coll1, coll2, coll3 = st.columns(3)
+
+coll1.metric("Strongest Event", strongest["magnitude"])
+coll2.metric("Tsunami Events", tsunami_count)
+coll3.metric("Total Reviewed", reviewed)
 
 st.subheader("Latest Earthquakes")
 st.dataframe(df.sort_values("event_time", ascending=False).head(10))
