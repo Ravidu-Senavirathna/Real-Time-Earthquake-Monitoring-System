875a58958ad4a4d1cd01143e2bcd684ef4b3f793
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-04-21 08:20:18 +0530

    decleaed more function in utils

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -2,7 +2,32 @@ import pandas as pd
 from database.db import engine
 import streamlit as st
 
-@st.cache_data
+@st.cache_data(ttl=300)
+
+
 def load_data():
-    query = "SELECT * FROM earthquakes"
-    return pd.read_sql(query, engine)
+    query = """
+        SELECT *
+        FROM earthquakes
+    """
+    df = pd.read_sql(query, engine)
+
+    df["event_time"] = pd.to_datetime(df["event_time"])
+
+    return df
+
+
+def get_top_earthquakes():
+    pass
+
+
+def get_tsunami_events():
+    pass
+
+
+def get_monthly_stats():
+    pass
+
+
+def get_recent_events():
+    pass
