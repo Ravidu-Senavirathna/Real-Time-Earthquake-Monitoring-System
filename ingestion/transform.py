df886759256a6bff8c61ced2dbfa4c5c28ef7f7f
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-05-04 18:02:23 +0530

    created a commin transform for both live and backfill

---
+++ ingestion/transform.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+def parse_earthquake(event):
+
+    props = event["properties"]
+    geo = event["geometry"]["coordinates"]
+
+    return {
+        "id": event["id"],
+
+        "magnitude": props.get("mag"),
+        "magnitude_type": props.get("magType"),
+
+        "place": props.get("place"),
+
+        "event_time": pd.to_datetime(
+            props["time"],
+            unit="ms"
+        ) if props.get("time") else None,
+
+        "updated_time": pd.to_datetime(
+            props["updated"],
+            unit="ms"
+        ) if props.get("updated") else None,
+
+        "latitude": geo[1],
+        "longitude": geo[0],
+        "depth": geo[2],
+
+        "tsunami": props.get("tsunami"),
+        "significance": props.get("sig"),
+
+        "status": props.get("status"),
+        "event_type": props.get("type")
+    }
